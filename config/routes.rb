cd9a30ba69e7882981e6318440202cee3de3905b
Rowan <rcumm017@uottawa.ca>
2017-03-24 00:40:41 +0000

    fixing up the login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :book_requests
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
+  get  '/form',    to: 'static_pages#form'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  get  '/login', to: 'static_pages#login'
   get '/started', to: 'static_pages#started'
 end
